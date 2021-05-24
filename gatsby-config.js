b8b9a9102fdb1bfe63ed70418f010fde78f4c90a
Oscar <hello@castaneda.io>
2021-05-24 13:34:39 -0500

    gatsby plugin image added

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,9 @@ module.exports = {
   plugins: [
     'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
       resolve: 'gatsby-source-sanity',
       options: {
