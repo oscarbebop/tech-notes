0b875f806ad9a4f3d3d3538abe5ebf28b69debee
Oscar <hello@castaneda.io>
2021-06-12 09:49:04 -0500

    testing home data

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -53,10 +53,6 @@ export default function Sidebar(): JSX.Element {
   const english: boolean = language === Lang.EN;
   const isTheThemeDark = theme === Theme.dark;
 
-  console.log('================');
-  console.log(isTheThemeDark);
-  console.log('================');
-
   return (
     <Container ItIsActive={activeSidebar}>
       <SidebarContainer
